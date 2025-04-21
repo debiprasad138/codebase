2764edc97ef7e9145198a9038cbd542339c10015
debiprasad138 <168815867+debiprasad138@users.noreply.github.com>
2025-04-21 12:55:41 +0000

    palindrome

---
+++ test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+bool isPalindrome(char str[]) {
+    int start = 0;
+    int end = strlen(str) - 1;
+
+    while (start < end) {
+        if (str[start] != str[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    if (isPalindrome(str)) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
+
+    return 0;
+}
